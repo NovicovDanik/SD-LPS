c19f3b0b742ad0dbeb35aefc75f4d394716a1100
NovicovDanik <90235070+NovicovDanik@users.noreply.github.com>
2022-05-03 18:09:36 +0300

    Add files via upload

--- Projects/Novicov pr6/actualizare.cpp
+++ Projects/Novicov pr6/actualizare.cpp
@@ -10,16 +10,16 @@ void Inscriere()
     char fam[15];
     ft = fopen("file.txt", "r");
     f = fopen("file.in", "wb");
-    while(fscanf(ft, "%d %s %s %d %d %d %s %d %d %s",
+    while(fscanf(ft, "%d %s %s %d %d %d %s %d %d",
                 &sport.id, sport.nume, fam, &sport.dn.zi, &sport.dn.luna, &sport.dn.an,
-                &sport.dis, &sport.inalt, &sport.greu, &sport.categoria)!= EOF)
+                &sport.dis, &sport.inalt, &sport.greu)!= EOF)
     {
         strcat(sport.nume, " ");
         strcat(sport.nume, fam);
         printf("\n%s", sport.nume);
         fwrite(&sport, sizeof(Sport),1, f);
     }
-    printf("Ok");
+    printf("\nOk");
     fclose(ft);
     fclose(f);
 }
@@ -29,28 +29,28 @@ void Linia (int st, int lin, int mij, int dr)
     printf("\n%c", st);
     for (i=1; i<=3; i++) printf("%c", lin); printf("%c", mij);
     for (i=1; i<=5; i++) printf("%c", lin); printf("%c", mij);
+    for (i=1; i<=25; i++) printf("%c", lin); printf("%c", mij);
     for (i=1; i<=15; i++) printf("%c", lin); printf("%c", mij);
-    for (i=1; i<=10; i++) printf("%c", lin); printf("%c", mij);
-    for (i=1; i<=20; i++) printf("%c", lin); printf("%c", mij);
+    for (i=1; i<=15; i++) printf("%c", lin); printf("%c", mij);
     for (i=1; i<=5; i++) printf("%c", lin); printf("%c", mij);
-    for (i=1; i<=7; i++) printf("%c", lin); printf("%c", mij);
+    for (i=1; i<=5; i++) printf("%c", lin); printf("%c", mij);
     for (i=1; i<=5; i++) printf("%c", lin); printf("%c", dr);
 }
 void Afisare()
 {
-     Sport sport;
+    Sport sport;
      FILE *f;
      int n = 0;
      f = fopen("file.in", "rb");
      printf("\n Lista initiala a sportivilor");
      Linia(201, 205, 209, 187);
-     printf("\n%c Nr%c ID %2c Numele sport %2cData nast.%cDisciplina%cInalt%cGreut%cCateg%c",
+     printf("\n%c Nr%c ID %2c Numele sportivilor %6cData nasterii%3cDisciplina%6cInalt%cGreut%cCateg%c",
             186, 179, 179, 179, 179, 179, 179, 179, 186);
      Linia(199, 196, 197, 182);
      while(fread(&sport, sizeof(Sport),1, f)>0)
      {
          n++;
-         printf("\n%c%2d %c %d %c%-15.15s%c%02d.%02d.%4d%c%-10.10s%c %3d %c %3d %c %3d %5c",
+         printf("\n%c%2d %c %d %c%-25.25s%c%02d.%02d.%4d%6c%-15.15s%c %3d %c %3d %c %d %c",
             186, n, 179, sport.id, 179, sport.nume, 179, sport.dn.zi,sport.dn.luna,sport.dn.an, 179, sport.dis ,179, sport.inalt, 179, sport.greu, 179, sport.categoria, 186);
      }
      Linia(200, 205, 207, 188);
@@ -58,7 +58,7 @@ void Afisare()
 }
 void Adaugare()
 {
-FILE *ft, *f;
+    FILE *ft, *f;
     Sport sport;
     char fam[15];
     ft = fopen("file2.txt", "r");
