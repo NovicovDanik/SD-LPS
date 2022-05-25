b8e3ac6dde7d10405c1868c123af7fe2022a7593
NovicovDanik <90235070+NovicovDanik@users.noreply.github.com>
2022-05-25 09:57:26 +0300

    valid

--- Projects/NovicovPr2/main.cpp
+++ Projects/NovicovPr2/main.cpp
@@ -5,21 +5,24 @@
 #define NMAX 1000000
 using namespace std;
 bool Palindrome(int n);
-long int Demlo(int n);
+long unsigned Demlo(int n);
 int main()
 {
-    int numar;
-    int jos, sus;
-    int contor, cit;
+    int numar = 3223;
+    int jos = 1, sus = 1000000;
+    int contor, cit = 1998, citd = 10;
     cout << "Proiect Nr. 2, elaborat de Novicov Daniil" << endl;
     cout << "Tema: Numere palindroame\n";
     //1. De verificat dacă numarul este palindrom
     cout << "Introduceti numarul intreg, pentru verificare: ";
-    cin >> numar;
+    //cin >> numar;
     cout << "Introduceti limitele de jos si de sus: ";
-    cin >> jos >> sus;
+    //cin >> jos >> sus;
     cout << "Cite numere palindroame: ";
-    cin >> cit;
+    //cin >> cit;
+    cout << "Cite numere Demlo: ";
+    //cin >> citd;
+
     if(Palindrome(numar))
         cout << "\n1. Numarul " << numar << " este Palindrom " << endl;
     else
@@ -30,8 +33,10 @@ int main()
     for(numar = jos, contor = 0; numar <= sus; numar++)
         if (Palindrome(numar))
         {
-            cout << setw(8) << numar ;
             contor++;
+            cout << setw(9) << numar ;
+            if (contor % 10 == 0)
+                cout << endl;
         }
     if(contor)
         cout << "\nIn total: " << contor << " numere\n";
@@ -40,28 +45,29 @@ int main()
     //3. De indicat primele n numere, care satisfac proprietatea de bază
 
     cout << "\n3. Primele "<< cit << " numere palindroame " << endl;
-    for(numar = 0, contor = 0; contor < cit; numar++)
+    for(numar = 1, contor = 0; contor < cit; numar++)
         if (Palindrome(numar))
         {
-            cout << setw(8) << numar ;
             contor++;
+            cout << setw(9) << numar ;
+             if (contor % 10 == 0)
+                cout << endl;
         }
     if(contor)
         cout << "\nIn total: " << contor << " numere\n";
     else
         cout << "Nu sunt numere palindroame pe interval";
+
     //4. De indicat toate numerele din intervalul [a; b], care satisfac proprietatea de bază (varianta optimizată) sau care satisfac a doua proprietate.
     cout << "\n4. Numere Demlo sunt:\n";
-    jos = 1;
-    sus = 10;
-    for (numar = jos; numar <= sus; numar++)
+    for (numar = 1; numar <= citd; numar++)
     {
 
         cout << "\t" << Demlo(numar) << endl;
 
     }
     //5. De determinat cel mai mare număr, care satisface condiţia indicată.
-    cout << "\n5. Cel mai mare numar palindrom este: ";
+    /*cout << "\n5. Cel mai mare numar palindrom este: ";
     for (numar = NMAX; numar > 0; numar--)
     {
         if (Palindrome(numar))
@@ -69,7 +75,7 @@ int main()
             cout << numar << endl;
             break;
         }
-    }
+    }*/
     return 0;
 }
 bool Palindrome(int n)
@@ -82,17 +88,18 @@ bool Palindrome(int n)
     }
     return (invers == n);
 }
-long int Demlo(int n)
+int Step (int a, int n)
 {
-    int power = 1;
+    int p = 1;
     for (int i = 1; i <= n; i++)
-        power *= 10;
-        //cout << (((power - 1) / 9)*((power - 1) / 9));
-        return (((power - 1) / 9)*((power - 1) / 9));
-    //return pow((power - 1)/ 9, 2);
- //pow(((pow(10, n) - 1) / 9), 2 );
- //return pow(((pow(10, n) - 1) / 9), 2 );
- //return ((pow((pow(10,n)- 1),2))/ 18);
+        p *= a;
+    return p;
+}
+long unsigned Demlo(int n)
+{
+    long unsigned power;
+    power = (Step(10, n) - 1)/9;
+    return power*power;
 }
 
 
